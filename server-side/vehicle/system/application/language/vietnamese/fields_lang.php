8432bdb264523953ec6d2315919572f839fcbf15
Trieu Nguyen <tantrieuf31@users.noreply.github.com>
2009-02-04 17:27:49 +0000

    add I18N vn

---
+++ server-side/vehicle/system/application/language/vietnamese/fields_lang.php
@@ -0,0 +1,235 @@
+<?php
+
+
+
+//////////////////  bao_duong_xe  ////////////////////
+
+// ------------------------------------------ //
+ $lang['bao_duong_xe'] = "Bảo dưỡng xe";
+// ------------------------------------------ //
+  $lang['bao_duong_xe.STT_XE'] = "STT";
+  $lang['bao_duong_xe.KHOANG_MUC_BAO_DUONG'] = "Khoảng mục bảo dưỡng";
+  $lang['bao_duong_xe.THOI_GIAN'] = "Thời gian";
+  $lang['bao_duong_xe.SPEEDOMETER'] = "KM đã đi";
+
+
+ ///////////////////////////////////////////////////////////
+//-----------------------------------------------------------------------------
+
+//////////////////  chi_nhanh  ////////////////////
+
+// ------------------------------------------ //
+ $lang['chi_nhanh'] = "Chi nhánh";
+// ------------------------------------------ //
+  $lang['chi_nhanh.MS_CHI_NHANH'] = "MS chi nhánh";
+  $lang['chi_nhanh.TEN_CHI_NHANH'] = "Tên chi nhánh";
+
+
+ ///////////////////////////////////////////////////////////
+//-----------------------------------------------------------------------------
+
+//////////////////  dieu_khoan_hop_dong  ////////////////////
+
+// ------------------------------------------ //
+ $lang['dieu_khoan_hop_dong'] = "Điều khoản hợp đồng";
+// ------------------------------------------ //
+  $lang['dieu_khoan_hop_dong.STT_DKHD'] = "STT";
+  $lang['dieu_khoan_hop_dong.MS_HOP_DONG'] = "MS hợp đồng";
+  $lang['dieu_khoan_hop_dong.LOAI_MODEL'] = "Loại model";
+  $lang['dieu_khoan_hop_dong.THOI_GIAN'] = "Thời gian";
+  $lang['dieu_khoan_hop_dong.KM_TRAN'] = "Km trần";
+  $lang['dieu_khoan_hop_dong.NGAY_BAT_DAU'] = "Ngày bắt đầu";
+  $lang['dieu_khoan_hop_dong.NGAY_KET_THUC'] = "Ngày kết thúc";
+  $lang['dieu_khoan_hop_dong.VI_TRI'] = "Vị trí";
+  $lang['dieu_khoan_hop_dong.DICH_VU'] = "Dịch vụ";
+  $lang['dieu_khoan_hop_dong.BAO_HIEM'] = "Bảo hiểm";
+  $lang['dieu_khoan_hop_dong.CHI_PHI_CHO_KM_THEM'] = "Chi phí cho Km thêm";
+  $lang['dieu_khoan_hop_dong.NHUONG_QUYEN_THUONG_MAI'] = "Nhượng quyền thương mại";
+
+
+ ///////////////////////////////////////////////////////////
+//-----------------------------------------------------------------------------
+
+//////////////////  gps_markers  ////////////////////
+
+// ------------------------------------------ //
+ $lang['gps_markers'] = null;
+// ------------------------------------------ //
+  $lang['gps_markers.ID'] = null;
+  $lang['gps_markers.STT_NKHT'] = null;
+  $lang['gps_markers.LAT'] = null;
+  $lang['gps_markers.LNG'] = null;
+  $lang['gps_markers.TYPE'] = null;
+  $lang['gps_markers.GPS_TIME'] = null;
+
+
+ ///////////////////////////////////////////////////////////
+//-----------------------------------------------------------------------------
+
+//////////////////  hop_dong_thue_xe  ////////////////////
+
+// ------------------------------------------ //
+ $lang['hop_dong_thue_xe'] = "Hợp đồng thuê Xe";
+// ------------------------------------------ //
+  $lang['hop_dong_thue_xe.SO_DANG_KY_XE'] = "Số ĐK xe";
+  $lang['hop_dong_thue_xe.MS_HOP_DONG'] = "MS hợp đồng";
+  $lang['hop_dong_thue_xe.MS_CHI_NHANH'] = "MS chi nhánh";
+  $lang['hop_dong_thue_xe.CHI_NHANH_QUAN_LY_XE'] = "Chi nhánh quản lý xe";
+  $lang['hop_dong_thue_xe.CO_QUAN_CHO_THUE_XE'] = "Cơ quan cho thuê";
+  $lang['hop_dong_thue_xe.HOP_DONG'] = "Mã hợp đồng";
+  $lang['hop_dong_thue_xe.LOAI_HOP_DONG'] = "Loại hợp đồng";
+  $lang['hop_dong_thue_xe.NGUOI_TIEP_NHAN'] = "Người tiếp nhận";
+  $lang['hop_dong_thue_xe.SO_TIEN_'] = "Số tiền";
+  $lang['hop_dong_thue_xe.SOTHUC_TE_'] = "Thực trả";
+  $lang['hop_dong_thue_xe.MS_THE_XANG'] = "MS thể xăng";
+  $lang['hop_dong_thue_xe.MS_PIN'] = "Mã số PIN";
+  $lang['hop_dong_thue_xe.MS_TAI_XE'] = "Mã số tài xế";
+
+
+ ///////////////////////////////////////////////////////////
+//-----------------------------------------------------------------------------
+
+//////////////////  lich_su_cap_nhat_xe  ////////////////////
+
+// ------------------------------------------ //
+ $lang['lich_su_cap_nhat_xe'] = "Lịch sử cập nhật Xe";
+// ------------------------------------------ //
+  $lang['lich_su_cap_nhat_xe.STT_XE'] = null;
+  $lang['lich_su_cap_nhat_xe.SO_DANG_KY_XE'] = null;
+  $lang['lich_su_cap_nhat_xe.STT_CAP_NHAT'] = null;
+  $lang['lich_su_cap_nhat_xe.NGAY_CAP_NHAT'] = null;
+  $lang['lich_su_cap_nhat_xe.TEN'] = null;
+  $lang['lich_su_cap_nhat_xe.GIA_TRI'] = null;
+  $lang['lich_su_cap_nhat_xe.KIEU_DU_LIEU'] = null;
+
+
+ ///////////////////////////////////////////////////////////
+//-----------------------------------------------------------------------------
+
+//////////////////  model_xe  ////////////////////
+
+// ------------------------------------------ //
+ $lang['model_xe'] = "Model Xe";
+// ------------------------------------------ //
+  $lang['model_xe.STT_MODEL_XE'] = "STT";
+  $lang['model_xe.MS_MODEL_XE'] = "Mã số model xe";
+  $lang['model_xe.LOAI_MODEL'] = "Loại model";
+  $lang['model_xe.NHAN_HIEU'] = "Nhãn hiệu";
+  $lang['model_xe.MS_THUE'] = "MS Thuế";
+  $lang['model_xe.FUEL'] = "Nhiên liệu";
+  $lang['model_xe.PTAC'] = "PTAC";
+  $lang['model_xe.PAYLOAD'] = "Trọng tải";
+  $lang['model_xe.FLOOR_AREA'] = "Diện tích sàn";
+  $lang['model_xe.GENRE'] = "Loai xe";
+  $lang['model_xe.SO_SUON'] = "Số sườn";
+  $lang['model_xe.KM_METER_AVAILABLE_'] = "Km đã đi";
+
+
+ ///////////////////////////////////////////////////////////
+//-----------------------------------------------------------------------------
+
+//////////////////  nhat_ki_hanh_trinh  ////////////////////
+
+// ------------------------------------------ //
+ $lang['nhat_ki_hanh_trinh'] = "Nhật ký hành trình";
+// ------------------------------------------ //
+  $lang['nhat_ki_hanh_trinh.STT_NKHT'] = "STT";
+  $lang['nhat_ki_hanh_trinh.STT_XE'] = "STT xe";
+  $lang['nhat_ki_hanh_trinh.TEN'] = "Tên";
+  $lang['nhat_ki_hanh_trinh.DIA_DIEM'] = "Đia điểm";
+  $lang['nhat_ki_hanh_trinh.LATITUDE'] = "LAT";
+  $lang['nhat_ki_hanh_trinh.LONGITUDE'] = "LONG";
+  $lang['nhat_ki_hanh_trinh.NGAY_KHOI_HANH'] = "Ngày khởi hành";
+  $lang['nhat_ki_hanh_trinh.NGAY_KET_THUC'] = "Ngày kết thúc";
+
+
+ ///////////////////////////////////////////////////////////
+//-----------------------------------------------------------------------------
+
+//////////////////  sys_sessions  ////////////////////
+
+// ------------------------------------------ //
+ $lang['sys_sessions'] = "Quản lý phiên làm việc";
+// ------------------------------------------ //
+  $lang['sys_sessions.username'] = null;
+  $lang['sys_sessions.session_id'] = null;
+  $lang['sys_sessions.ip_address'] = null;
+  $lang['sys_sessions.user_agent'] = null;
+  $lang['sys_sessions.last_activity'] = null;
+
+
+ ///////////////////////////////////////////////////////////
+//-----------------------------------------------------------------------------
+
+//////////////////  thiet_bi  ////////////////////
+
+// ------------------------------------------ //
+ $lang['thiet_bi'] = "Thiết bị";
+// ------------------------------------------ //
+  $lang['thiet_bi.MS_THIET_BI'] = "MS Thiết bị";
+  $lang['thiet_bi.TEN_THIET_BI'] = "Tên thiết bị";
+  $lang['thiet_bi.LOAI_THIET_BI'] = "Loại thiết bị";
+  $lang['thiet_bi.SO_DANG_KY_XE'] = "Số ĐK xe";
+
+
+ ///////////////////////////////////////////////////////////
+//-----------------------------------------------------------------------------
+
+//////////////////  thuoc_tinh_mo_rong  ////////////////////
+
+// ------------------------------------------ //
+ $lang['thuoc_tinh_mo_rong'] = null;
+// ------------------------------------------ //
+  $lang['thuoc_tinh_mo_rong.MS_HOP_DONG'] = null;
+  $lang['thuoc_tinh_mo_rong.TEN'] = null;
+  $lang['thuoc_tinh_mo_rong.STT_XE'] = null;
+  $lang['thuoc_tinh_mo_rong.GIA_TRI'] = null;
+
+
+ ///////////////////////////////////////////////////////////
+//-----------------------------------------------------------------------------
+
+//////////////////  thuoc_tinh_mo_rong_cua_chi_nhanh  ////////////////////
+
+// ------------------------------------------ //
+ $lang['thuoc_tinh_mo_rong_cua_chi_nhanh'] = "Thuộc tích mở rộng của chi nhánh";
+// ------------------------------------------ //
+  $lang['thuoc_tinh_mo_rong_cua_chi_nhanh.MS_CHI_NHANH'] = "MS chi nhánh";
+  $lang['thuoc_tinh_mo_rong_cua_chi_nhanh.TEN'] = "Tên chi nhánh";
+  $lang['thuoc_tinh_mo_rong_cua_chi_nhanh.GIA_TRI'] = "Giá trị";
+
+
+ ///////////////////////////////////////////////////////////
+//-----------------------------------------------------------------------------
+
+//////////////////  thuoc_tinh_mo_rong_cua_xe  ////////////////////
+
+// ------------------------------------------ //
+ $lang['thuoc_tinh_mo_rong_cua_xe'] = "Thuộc tích mở rộng của xe";
+// ------------------------------------------ //
+  $lang['thuoc_tinh_mo_rong_cua_xe.STTXE'] = "STT xe";
+  $lang['thuoc_tinh_mo_rong_cua_xe.TEN'] = "Tên";
+  $lang['thuoc_tinh_mo_rong_cua_xe.GIA_TRI'] = "Giá trị";
+
+
+ ///////////////////////////////////////////////////////////
+//-----------------------------------------------------------------------------
+
+//////////////////  xe  ////////////////////
+
+// ------------------------------------------ //
+ $lang['xe'] = "Xe";
+// ------------------------------------------ //
+  $lang['xe.STT_XE'] = "STT xe";
+  $lang['xe.SO_DANG_KY_XE'] = "Số ĐK";
+  $lang['xe.MS_MODEL_XE'] = "Số model xe";
+  $lang['xe.MS_THIET_BI'] = "MS Thiết bị";
+  $lang['xe.THE_TICH_THAT'] = "Thể tích thật";
+  $lang['xe.NGAY_CAP_NHAT'] = "Ngày cập nhật";
+
+
+ ///////////////////////////////////////////////////////////
+//-----------------------------------------------------------------------------
+
+
+?>
